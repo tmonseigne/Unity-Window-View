7b1da4668a3656148bfe8e699efc7c469a3044fe
Thibaut Monseigne <thibaut.monseigne@inria.fr>
2021-08-13 11:57:48 +0200

    :sparkles: template Check

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	[SerializeField] private float _speed = 50;
+
+	// Update is called once per frame
+	void Update() { transform.Rotate(new Vector3(_speed, 0, 0) * Time.deltaTime); }
+}
